3e5b363d79587f4030c25ea33706ea2e41e3aa8c
josevictorr15 <victorsantanaa2011@hotmail.com>
2024-07-12 10:43:56 -0300

    Criando a funcionalidade de excluir e marcar tarefas feitas/não feitas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,59 @@
+/* eslint-disable no-unused-vars */
 import './Todo.scss';
+import { useState } from 'react';
 
-function Todo({ title, description, isDone }) {
+function Todo()
+{
 
-  return (
-    <>
-        <div className={`todo ${isDone ? 'done' : ''}`}>
-            <button className="erase">x erase</button>
-            <h3>
-                {title}
-            </h3>
-            <p>
-                {description}
-            </p>
-            <div className="task-check">
-                <input type="checkbox" checked={isDone} />
-                <label>
-                    {!isDone ? 'To-Do' : 'Done'}
-                </label>
-            </div>
-        </div>
+    const initialTodos = [
+        { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+        { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+        { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+    ];
+    
+    const [todos, setTodos] = useState(initialTodos);
 
-    </>
-  )
+    const deleteHandler = (id) => {
+        if(confirm('Are you sure you want to delete the to-do?')){
+            setTodos(todos.filter(todo => todo.id !== id));
+        }
+    }
+
+    const isDoneHandler = (id) => {
+        setTodos(todos.map(todo => {
+            if(todo.id === id){
+                todo.isDone = !todo.isDone
+                return todo;
+            } else {
+                return todo;
+            }
+        }))
+    }
+
+    return (
+        <>
+            {todos.map(todo => 
+                <div className={`todo ${todo.isDone ? 'done' : ''}`} key={todo.id}>
+                    <button className="erase" onClick={() => deleteHandler(todo.id)}>x erase</button>
+                    <h3>
+                        {todo.title}
+                    </h3>
+                    <p>
+                        {todo.description}
+                    </p>
+                    <div className="task-check">
+                        <input type="checkbox" defaultChecked={todo.isDone} onClick={() => isDoneHandler(todo.id)}/>
+                        <label>
+                            {!todo.isDone ? 'To-Do' : 'Done'}
+                        </label>
+                    </div>
+                </div>
+            )}
+
+
+
+        </>
+    )
 }
 
 export default Todo
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,24 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import Todo from './Todo.jsx';
 
 function TodosList() {
 
+
+
+
+
   return (
     <>
-        <div className="todos">
-
-            <Todo
-              title="Do Groceries"
-              description="Buy apples, rice, juice and toilet paper."
-              isDone={true}
-            />
-
-            <Todo
-              title="Study React"
-              description="Understand context, reducers and state management with Redux."  
-              isDone={false}
-            />
-
-        </div>
     </>
   )
 }
